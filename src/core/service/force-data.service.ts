fe502645c7c09bf8f4f0533057c6731abe316f33
Hayden Yu <hayden.yu.toronto@gmail.com>
2019-05-18 15:33:57 -0400

    query with paging

--- src/core/service/force-data.service.ts
+++ src/core/service/force-data.service.ts
@@ -1,48 +1,46 @@
 import { force as sf } from '../../config/force';
 import { logger } from '../../config/logger';
+import { QueryResult } from 'jsforce';
 
 export class ForceDataService {
-  private static queryMore(locator: string): Promise<any[]> {
+
+  public static query(query: string): Promise<QueryResult<any>> {
     return new Promise((resolve, reject) => {
       try {
-        sf.queryMore(locator, undefined, (err, res) => {
+        sf.query(query, undefined, (err, res) => {
           if (err) {
             return reject(err);
           }
-          if (!res.done) {
-            return this.queryMore(<string>res.nextRecordsUrl)
-              .then((records: any[]) => resolve(res.records.concat(records)))
-              .catch(reject);
-          }
-          resolve(res.records);
+          return resolve(res);
         });
       } catch (err) {
         logger.error(err);
         reject(err);
       }
-    });
+    })
   }
 
-  public static query(query: string): Promise<any> {
+  public static queryNext(prev: QueryResult<any>): Promise<QueryResult<any>> {
     return new Promise((resolve, reject) => {
+      if (!prev.nextRecordsUrl) {
+        return resolve({
+          records: [],
+          done: true,
+          totalSize: 0,
+        });
+      }
       try {
-        sf.query(query, undefined,
-          (err, res) => {
-            if (err) {
-              return reject(err);
-            }
-            if (!res.done) {
-              return this.queryMore(<string>res.nextRecordsUrl)
-                .then(records => resolve(res.records.concat(records)))
-                .catch(reject);
-            }
-            resolve(res.records);
-          });
+        sf.queryMore(prev.nextRecordsUrl, undefined, (err, res) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(res);
+        });
       } catch (err) {
         logger.error(err);
         reject(err);
       }
-    });
+    })
   }
 
   public static getRecentUpdates(objectName: string, from: string, to: string): Promise<string[]> {
