160b0a3b364266563c73cb745dc8786601b98650
hayden <hayden.yu.toronto@gmail.com>
2019-09-13 14:42:15 -0400

    postgres transaction

--- src/lib/database/postgres.ts
+++ src/lib/database/postgres.ts
@@ -1,4 +1,4 @@
-import { Pool, QueryResult, PoolConfig, types } from 'pg';
+import { Pool, QueryResult, PoolConfig, types, ClientBase } from 'pg';
 import { Logger, loggerPlaceHolder } from '../Logger';
 import { EventEmitter } from 'events';
 
@@ -36,33 +36,61 @@ export class Postgres {
     });
   }
 
-  connect() {
-    return this.client.connect();
-  }
-
-  disconnect() { // TODO: this.client.end here is not terminating connections, need to investigate
+  disconnect() {
     this.logger.debug(`disconnecting pg pool with ${this.activeWorker} job running and ${this.queue.length} left in queue`);
     return this.client.end();
+  }
+
+  _query(task: QueryRequest) {
+    const query = task.query as string;
+    this.client.query(query, task.params || [], (err, result) => {
+      --this.activeWorker;
+      this.events.emit('dequeue');
+      this.logger.silly('pg query finished');
+      if (err) {
+        this.logger.debug(query);
+        this.logger.error(err);
+        return task.reject(err);
+      }
+      task.resolve(result);
+    })
+  }
+
+  async _transact(task: QueryRequest) {
+    const transaction = task.transaction as Transaction;
+    const conn = await this.client.connect();
+    try {
+      await conn.query('BEGIN');
+      this.logger.info('begin transaction');
+      await transaction(new Connection(conn, this.logger));
+      this.logger.info('commit transaction');
+      await this.query('COMMIT');
+      task.resolve();
+    } catch (err) {
+      this.logger.error(err);
+      this.logger.info('rollback transaction');
+      await this.query('ROLLBACK');
+      task.reject(err);
+    } finally {
+      await conn.release();
+      --this.activeWorker;
+      this.events.emit('dequeue');
+    }
   }
 
   private _dequeue() {
     if (this.MAX_POOL_SIZE > this.activeWorker) {
       const task = this.queue.shift();
       if (task) {
-        this.logger.silly(`picked up pg query`)
+        this.logger.silly(`picked up pg query`);
         try {
           ++this.activeWorker;
-          this.client.query(task.query, task.params || [], (err, result) => {
-            --this.activeWorker;
-            this.events.emit('dequeue');
-            this.logger.silly('pg query finished');
-            if (err) {
-              this.logger.debug(task.query);
-              this.logger.error(err);
-              return task.reject(err);
-            }
-            task.resolve(result);
-          })
+          if (task.transaction) {
+            return this._transact(task);
+          }
+          if (task.query) {
+            return this._query(task);
+          }
         } catch (err) {
           task.reject(err);
         }
@@ -72,8 +100,32 @@ export class Postgres {
 }
 
 export type QueryRequest = {
-  query: string,
+  transaction?: Transaction,
+  query?: string,
   params?: any[],
   resolve: (value?: QueryResult | PromiseLike<QueryResult>) => void,
   reject: (reason?: any) => void,
 }
+
+class Connection {
+  private client: ClientBase;
+  private logger: Logger;
+
+  constructor(client: ClientBase, logger: Logger) {
+    this.client = client;
+    this.logger = logger;
+  }
+
+  query(qryStr: string, values?: any[]): Promise<QueryResult> {
+    return new Promise((resolve, reject) => this.client.query(qryStr, values || [], (err, res) => {
+      if (err) {
+        this.logger.debug(qryStr);
+        this.logger.error(err);
+        return reject(err);
+      }
+      return resolve(res);
+    }))
+  }
+}
+
+export type Transaction = (conn: Connection) => Promise<void>
